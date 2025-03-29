442b957e9ae7c25260441cb799cfdbeeb2bae2e5
CrisJauregi <crisjauregui@MacBook-Air-de-Cris.local>
2025-03-28 19:00:52 -0500

    inicio cardlist.js

---
+++ src/components/cuadro/cardlist.js
@@ -0,0 +1,12 @@
+import cardList from "./cardlist.js";
+
+class CategoriasLists extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  }
+export default cardLists;
+
+  
